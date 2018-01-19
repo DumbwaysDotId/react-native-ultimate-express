d9f16db22e960d3b15343ffe7cbc211b4fa6586e
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-01-20 05:17:01 +0700

    40.handling-request

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', function(req, res){
+  res.send({
+    hello: "world"
+  })
+})
+
 app.listen(5000, function(){
   console.log('express app now listening on port 5000')
 })
